12951aa5ec5d55efb991d5d361e73f474f5c1dae
Spet77 <76452706+Yelloo5191@users.noreply.github.com>
2024-07-07 21:49:47 -0700

    yuh

--- settings/config.py
+++ settings/config.py
@@ -1,3 +1,11 @@
+import pygame
 RESOLUTION = (1280, 720)
 DISPLAY_SIZE = (320, 180)
 FPS = 60
+
+CONTROLS = {
+    "up": pygame.K_w,
+    "down": pygame.K_s,
+    "left": pygame.K_a,
+    "right": pygame.K_d,
+}
